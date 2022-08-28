42d4b93cd2c53d6c574fc70d548c134069011dd6
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-08-28 17:27:30 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let buildToDo = document.getElementById("new-task-description").value;
+
+  let list = document.createElement("p");
+  let button = document.createElement("button");
+  button.addEventListener("click", (e) => {
+    e.target.parentNode.remove();
+  });
+  button.textContent = 'x';
+
+  list.textContent = `${buildToDo} `;
+  list.appendChild(button);
+  document.querySelector("#tasks").appendChild(list);
+  document.querySelector("h1").remove();
+
 });
+
+});
+
+ 
